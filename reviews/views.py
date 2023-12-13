b82c834b42d92c831132d934d573fe417f642a97
Marv <marvade8@gmail.com>
2023-12-13 06:34:10 +0100

    Update

--- reviews/views.py
+++ reviews/views.py
@@ -4,11 +4,33 @@ from django.shortcuts import render
 from .forms import ReviewForm
 from .models import Review
 
+from django.views import View
+
 # Create your views here.
+
+class ReviewView(View):
+    def get(self, request):
+        form = ReviewForm()
+        
+        return render(request, "reviews/review.html", {
+        "form":form
+    })
+        
+    def post(self, request):
+        form = ReviewForm(request.POST,)
+        
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank_you")
+        
+        return render(request, "reviews/review.html", {
+        "form":form
+    })
+    
 
 def review(request):
     if request.method == 'POST':
-        form = ReviewForm(request.POST, instance)
+        form = ReviewForm(request.POST,)
         
         if form.is_valid():
             form.save()

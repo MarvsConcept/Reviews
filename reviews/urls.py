5e7792421d95243f51dd4b69141b79f95d6770da
Marv <marvade8@gmail.com>
2023-12-19 05:45:30 +0100

    Update

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.ReviewView.as_view(),),
     path('thank_you', views.thank_youView.as_view(),),
     path("reviews", views.ReviewListView.as_view()),
-    path("reviews/<int:pk>", views.ReviewDetailView.as_view())
+    path("reviews/favorite", views.AddFavoriteView.as_view()),
+    path("reviews/<int:pk>", views.ReviewDetailView.as_view()),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -92,6 +92,14 @@ class ReviewDetailView(DetailView):
     template_name = "reviews/review_detail.html"
     model = Review
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id ==  str(loaded_review.id)
+        return context
+        
 # class ReviewDetailView(TemplateView):
 #     template_name = "reviews/review_detail.html"
     
@@ -101,3 +109,9 @@ class ReviewDetailView(DetailView):
 #         selected_review = Review.objects.get(pk=review_id)
 #         context["review"] = selected_review
 #         return context
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST["review_id"]
+        request.session["favorite_review"] = review_id
+        return HttpResponseRedirect("/reviews/" + review_id)
